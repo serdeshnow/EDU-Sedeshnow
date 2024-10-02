1c77e6801816462c59dca13874a8b1717d35c1e4
serdeshnow <vserdes1977@mail.ru>
2024-10-02 18:47:35 +0300

    upd: make homework1

---
+++ src/routes/homework1/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="js">
+	let date = "7.10.2024";
+	const сascadingStyleSheets = "Ждем вас в четверг на дистанте!";
+	let spoiler = `А также ${date}!`;
+</script>
+
+<div>
+	<h1>... Тут написана ваша домашняя работа! ...</h1>
+	<p id="css">{сascadingStyleSheets}</p>
+	<p class="spoiler">{spoiler}</p>
+</div>
+
+<style lang="css">
+	div {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	div p {
+		font-size: 20px;
+	}
+
+	#css {
+		color: blue;
+	}
+
+	.spoiler {
+		border: 1px solid orange;
+	}
+</style>
